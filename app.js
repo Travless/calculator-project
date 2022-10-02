1073e19b2b44ea0571b96a34ee0d2478319e2337
Travless <travis.white0101@gmail.com>
2022-10-02 14:29:17 -0400

    change approach in js to incorporate classes to help steamline code and provide a cleaner experience for other developers

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 let displayValue;
 let equation = [];
 
+
+class Calculator {
+    constructor(calcDisplay) {
+        this.calcDisplay = calcDisplay;
+        this.clear();
+    }
+
+    appendNumber() {
+
+    }
+
+    clear() {
+        this.calcDisplay = '';
+        this.operation = undefined;
+    }
+
+    operationChoice(operation) {
+
+    }
+
+    equals() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
 let calcDisplay = document.getElementById('input-display');
 // calcDisplay.textContent = displayValue;
 
@@ -8,25 +37,46 @@ let calcButtons = document.getElementsByClassName('calc-button operator');
 
 const numButtons = document.getElementById('buttons-container');
 
-const add = function(a, b) {
-    let sum = a + b;
-    return sum;
-}
+const clearButton = document.getElementById('clear');
 
-const subtract = function(a, b) {
-    let difference = a - b;
-    return difference;
-}
+const divideButton = document.getElementById('divide');
 
-const multiply = function(a, b) {
-    let product = a * b;
-    return product;
-}
+const multiplyButton = document.getElementById('multiply');
 
-const divide = function(a, b) {
-    let remainder = a / b;
-    return remainder;
-}
+const subtractButton = document.getElementById('subtract');
+
+const additionButton = document.getElementById('addition');
+
+const equalsButton = document.getElementById('equals');
+
+const calculator = new Calculator(calcDisplay);
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(buttton.textContent);
+        calculator.updateDisplay();
+    })
+})
+
+// const add = function(a, b) {
+//     let sum = a + b;
+//     return sum;
+// }
+
+// const subtract = function(a, b) {
+//     let difference = a - b;
+//     return difference;
+// }
+
+// const multiply = function(a, b) {
+//     let product = a * b;
+//     return product;
+// }
+
+// const divide = function(a, b) {
+//     let remainder = a / b;
+//     return remainder;
+// }
 
 const operator = function (op, a, b) {
     let operator = op;
